df7f9ff74b21f5895cc81f5e6bb16ade26cf0717
Aashwalayan <aashwalayanjha@gmail.com>
2026-06-27 16:01:40 +0530

    changed title and port

--- server.ts
+++ server.ts
@@ -22,7 +22,7 @@ dotenv.config();
 connectDB();
 
 const app = express();
-const PORT = 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 // Increase body limit for base64 image uploads
 app.use(express.json({ limit: "20mb" }));
